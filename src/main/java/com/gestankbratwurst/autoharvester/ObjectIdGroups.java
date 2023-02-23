5855a3f37312cccf0b6305f0f3d45da2b2b3f3da
Flo0 <flo.roma@web.de>
2023-02-23 17:45:47 +0100

    Changed ore loc to coal

--- src/main/java/com/gestankbratwurst/autoharvester/ObjectIdGroups.java
+++ src/main/java/com/gestankbratwurst/autoharvester/ObjectIdGroups.java
@@ -11,6 +11,7 @@ public class ObjectIdGroups {
     create(10943, 11161);
     create(11365, 11364);
     create(11360, 11361);
+    create(11366, 11367);
   }
 
   public static int[] groupOrId(int id) {
